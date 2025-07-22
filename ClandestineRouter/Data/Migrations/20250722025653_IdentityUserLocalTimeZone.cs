44b8d36f9a5e3ccf2aa900335364ecb7a32a868e
Jonathan Williams <19499981+jonwillcode@users.noreply.github.com>
2025-07-21 23:04:57 -0400

    Add local time zone to the application user model

---
+++ ClandestineRouter/Data/Migrations/20250722025653_IdentityUserLocalTimeZone.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClandestineRouter.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityUserLocalTimeZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocalTimeZone",
+                table: "AspNetUsers",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalTimeZone",
+                table: "AspNetUsers");
+        }
+    }
+}
